c6aad3db241a1d2e0ff1ba416f50b45f6d2891e7
Nathaniel Lintz <nlintz@mail.csuchico.edu>
2016-10-10 21:44:03 -0700

    added functionality code that  will verify if username and password pass the criteria

--- form-validation.js
+++ form-validation.js
@@ -1,19 +1,28 @@
 function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
+  var a = userEntered.length;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
+
+  if(a >= 6){
+    document.getElementById("usernameGroup").classList.add("has-success");
+}
+  else {
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
-
-  //Show message that there is an error with the password...
+}
+  if(passEntered !== "password"){
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
+  else {
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
 }
