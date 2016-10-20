7e183855c694c92111115eb0adccf1eb9002c445
Nathaniel Lintz <nlintz@mail.csuchico.edu>
2016-10-19 18:55:56 -0700

    added automatic validation, alert for username/password

--- form-validation.js
+++ form-validation.js
@@ -1,27 +1,38 @@
-function validate(){
-  //Grab the user's input and store in variables
+function register(){
+  window.alert("Username: " + document.getElementById("user").value + '\n' + "Password: " + document.getElementById("pass").value);
+
+}
+
+function validateUsername(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
   var passLength = passEntered.length;
-
   if(/\s/.test(userEntered)){
   document.getElementById("usernameError").innerHTML="Username can't have spaces.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   document.getElementById("usernameGroup").classList.add("has-error");
+  document.getElementById("usernameGroup").classList.remove("has-success");
 }
   else if(userEntered.length < 6){
   document.getElementById("usernameError").innerHTML="Username must have at least 6 characters.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   document.getElementById("usernameGroup").classList.add("has-error");
+  document.getElementById("usernameGroup").classList.remove("has-success");
 }
 else{
   document.getElementById("usernameGroup").classList.add("has-success");
+  document.getElementById("usernameGroup").classList.remove("has-error");
 }
-
+}
+function validatePassword(){
+  var passEntered = document.getElementById("pass").value;
+  var passLength = passEntered.length;
+  var userEntered = document.getElementById("user").value;
   if(passEntered !== "password" && passEntered !== "Password" && passLength >=6 && passLength <=20 && passEntered !== userEntered){
     document.getElementById("passwordGroup").classList.add("has-success");
+    document.getElementById("passwordGroup").classList.remove("has-error");
   }
   else{
     if(passEntered == "password" || passEntered == "Password"){
@@ -41,5 +52,6 @@ else{
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   document.getElementById("passwordGroup").classList.add("has-error");
+  document.getElementById("passwordGroup").classList.remove("has-success");
 }
 }
